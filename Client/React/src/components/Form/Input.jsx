0819de2402dc68a24390ca526759070ec482f8cf
iigrow <iigrow@iigrowdeMacBook-Pro.local>
2016-07-03 23:38:43 +0800

    feat (login): add login module

---
+++ Client/React/src/components/Form/Input.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Input extends React.Component {
+  render () {
+    // TODO: 自定义前面的图标 获取焦点内容不为空时 显示删除按钮
+  }
+}
+
+export default Input;
---
+++ Client/React/src/components/Form/Password.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Password extends React.Component{
+  render () {
+    // TODO:自定义前面的图标 内容不为空并获得焦点时 显示眼睛按钮
+  }
+}
+
+export default Password;
