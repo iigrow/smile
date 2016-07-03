02297959df01fa448362cbb56c0ababef8f09d44
iigrow <iigrow@iigrowdeMacBook-Pro.local>
2016-07-03 23:38:43 +0800

    feat (login): add login module

---
+++ Client/React/src/components/Authenticate/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Input from '../Form/Input';
+
+class Login extends React.Component{
+  render () {
+    return (
+      <div>
+        <Input />
+        <Password />
+      </div>
+    );
+  }
+}
+
+export default Login;
