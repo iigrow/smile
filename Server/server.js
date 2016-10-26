557d9996d26dee05e233823457aaa970c82de77b
iigrow <star@mac.local>
2016-10-26 09:52:43 +0800

    feat: use https

--- Server/server.js
+++ Server/server.js
@@ -1,3 +1,12 @@
+/*
+ * @Author: star 
+ * @Date: 2016-10-26 09:29:54 
+ * @Last Modified by: star
+ * @Last Modified time: 2016-10-26 09:49:29
+ */
+
+var https = require('https');
+var ssl = require('koa-ssl');
 var app = require('koa')();
 var Router = require('./router');
 var authenticate = require('./filter/authenticate');
@@ -9,11 +18,6 @@ var router = Router({
 });
 // var formidable = require('formidable'); // 流式解析，能随着数据块的上传接受它们，解析它们，并吐出特定的部分，就像我们之前提到的部分请求头和请求主体
 
-console.log('start server ...')
-var env = 'dev'
-var port = env === 'dev' ? 3000 : 80;
-
-
 // app.keys = ['im a newer secret', 'i like turtle'];
 // app.keys = new KeyGrip(['im a newer secret', 'i like turtle'], 'sha256');
 
@@ -21,6 +25,14 @@ app.use(router.routes())
   .on('error', function (err, ctx) {
     console.log(err);
   })
-  .listen(port);
+
+console.log('start server ...')
+
+https.createServer({
+  ca: fs.readFileSync('/root/.ssl/ca.crt'),
+  key: fs.readFileSync('/root/.ssl/smilplex.key'),
+  cert: fs.readFileSync('/root/.ssl/smilplex.crt'),
+  passphrase: '1234'
+}, app.callback()).listen(Process.env.PORT);
 
 console.log('waiting request ...')
